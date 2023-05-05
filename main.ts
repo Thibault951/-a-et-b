26d0fce32b7d90b0d5ebcb165ad1fa5b1c3429cf
Thibault951 <132680158+Thibault951@users.noreply.github.com>
2023-05-05 17:03:02 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (1 + 0))
+    music.playMelody("C5 B F A B D E C ", 40)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (1 + 1))
+    music.playMelody("G B A G C5 B A B ", 120)
 })
